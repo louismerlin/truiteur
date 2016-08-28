b06163dba655e425b9ba1c221e4ceb4e7ca8a6a2
Othman BENCHEKROUN <othman.benchekroun@epfl.ch>
2016-08-28 16:55:50 +0200

    Implement tuite related functions

--- api/api.go
+++ api/api.go
@@ -2,17 +2,40 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
+    "net/url"
     "net/http"
+    "encoding/json"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    http.HandleFunc("/truite", func(w http.ResponseWriter, r *http.Request) {
+        if r.Method == "POST" {
+			addTruite(r.PostForm)
+		} else if r.Method == "GET" {
+			if err := json.NewEncoder(w).Encode(getTruite()); err != nil {
+				panic(err)
+			}
+		}
     })
 
     log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
+
+type Truite struct {
+	Id 		int		`json:"id"`
+	Body 	string	`json:"body"`
+}
+
+func addTruite(data url.Values){
+	fmt.Printf("Post!")
+}
+
+func getTruite() Truite {
+	return Truite{Id: 1, Body: "Hello, world!"}
+}
+
+type Truites []Truite
+
 
