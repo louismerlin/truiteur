f94f56429ef66f1a6f0395a377c60df34543789c
Othman BENCHEKROUN <othman.benchekroun@epfl.ch>
2016-08-31 18:12:11 +0200

    Definition of Fisherman and encrypt password

--- api/truiteur/fisherman.go
+++ api/truiteur/fisherman.go
@@ -1,1 +1,24 @@
 package truiteur
+
+import "golang.org/x/crypto/bcrypt"
+
+type Fisherman struct {
+	Username   	string  `json:"username"`
+	Password 	string 	`json:"password"`
+	Truites		Truite[] `json:"truites"`
+}
+
+func AddFisherman(data url.Values) {
+	
+}
+
+func encryptPassword(password string) string {
+	pswd := []byte(password)
+	
+	hashedPassword, err := bcrypt.GenerateFromPassword(pswd, bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	
+	return hasehdPassword
+}
